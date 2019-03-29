3cc4097808848ce9532be8147926b4357d0fb743
Gitea <gitea@fake.local>
2019-03-29 11:00:17 +0800

    merge straem

--- client/config/dev.env.js
+++ client/config/dev.env.js
@@ -4,7 +4,7 @@ const prodEnv = require('./prod.env')
 
 
 // 本地运行时server端的地址
-var devServerHost = '"http://127.0.0.1:9898"'
+var devServerHost = '"http://192.168.10.158:9898"'
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
